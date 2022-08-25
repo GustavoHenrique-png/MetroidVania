58d8da9e48879a07b4217534ae3d227e01e66422
Gustavo Henrique <ymooncake@gmail.com>
2022-08-24 21:03:54 -0300

    Update Player.gd

--- MetroidVania/Assets/Scripts/Player.gd
+++ MetroidVania/Assets/Scripts/Player.gd
@@ -1,20 +1,18 @@
-extends KinematicBody2D
+extends KinematicBody2D#extendendo kinematic body
 
-var velocity = Vector2()
-var moveSpeed = 400
+var velocity = Vector2()#Variavel que vai colocar a velociadade em plano cartesianno
+var moveSpeed = 400#Velocidade de movimento
 
-func _physics_process(delta):
-	
-	_get_inputs()
-	
-	move_and_slide(velocity)
+func _physics_process(delta):#função que ocorre o tempo todo
+	_get_input()#chamada da função que pega os inputs
+	move_and_slide(velocity)#função necessária para o movimento 
 
-func _get_inputs():
-	velocity.x = 0
-	
+
+func _get_input():#função que pega os imputs do teclado
+	velocity.x = 0#setando velocidade 0 para a escolha de animações
+	#pegando a direção de movimento como um inteiro e segundo o plano cartesiano, positivo direita, negativo esquerda
 	var moveDirection = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	
-	velocity.x = lerp(velocity.x, moveDirection*moveSpeed,0.2)
-	
-	if moveDirection !=0:
+	velocity.x = lerp(velocity.x, moveSpeed*moveDirection/2, 0.2)#
+	#Se a velocidade for diferente de zero ele vira o sprite, positivo vira pra direta, negativo pra esquerda
+	if (moveDirection !=0):
 		$Sprite.scale.x = moveDirection
