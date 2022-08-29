2a0b44c39b2b01658834887fdb59ac59fc1a885c
Gustavo Henrique <ymooncake@gmail.com>
2022-08-29 13:37:39 -0300

    jump create

--- MetroidVania/Assets/Scripts/Player.gd
+++ MetroidVania/Assets/Scripts/Player.gd
@@ -1,12 +1,18 @@
-extends KinematicBody2D#extendendo kinematic body
+extends KinematicBody2D
 
-var velocity = Vector2()#Variavel que vai colocar a velociadade em plano cartesianno
-var moveSpeed = 400#Velocidade de movimento
-var gravity = 3000#Gravidade
+var velocity = Vector2()#colocando velocidade vetorial
+var moveSpeed = 480#velocidade de movimento do personagem
+var gravity = 1000#força da gravidade
+var jumpForce = -720#força do pulo(gavidade-forçapulo=pulo)
+var isGrounded#variavel que verifica se ta no chão
+onready var raycasts = $raycasts#acessando o nó raycast(no que checa o chão)
 
 func _physics_process(delta):#função que ocorre o tempo todo
-	velocity.y = gravity*delta#Calculo da velocidade em y(gravidade)
 	_get_input()#chamada da função que pega os inputs
+	#_set_animation()#chamada da função que escolhe as animções
+	isGrounded = _check_is_grounded()#chamada da função que verifica o chão
+	
+	velocity.y += gravity*delta#calculo da gravidade(gravidade X tempo decorrido)
 	move_and_slide(velocity)#função necessária para o movimento 
 
 
@@ -18,3 +24,25 @@ func _get_input():#função que pega os imputs do teclado
 	#Se a velocidade for diferente de zero ele vira o sprite, positivo vira pra direta, negativo pra esquerda
 	if (moveDirection !=0):
 		$Sprite.scale.x = moveDirection
+
+
+func _input(event):#função de pulo
+	if(event.is_action_pressed("ui_up")and isGrounded):#se foi pressionada e está no chão
+		velocity.y = jumpForce/2#calculo do pulo
+
+
+func _check_is_grounded():#função de checagem do pulo
+	for raycast in raycasts.get_children():#acessando os nós do raycast
+		if raycast.is_colliding():#se estiver tocando o chão retorna verdade, possibilitando o pulo
+			return true
+	return false#Se não estiver tocando o chão(ar) retorna falso impossibilitando o pulo
+
+
+func _set_animation(): #função que seta as animações
+	var anim = "idleAnim"
+	if !isGrounded:#se estiver no ar(diferente de no chão) seta a animação de pulo
+		anim = "jumpAnim"
+	elif velocity.x != 0:#se a velocidade for diferente de 0 seta a animação de corrida
+		anim = "runAnim"
+	if $AnimationPlayer.assigned_animation != anim:#?
+		$AnimationPlayer.play(anim)#toca a animação
