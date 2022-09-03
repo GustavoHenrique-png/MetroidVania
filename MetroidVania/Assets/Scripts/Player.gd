8ab8ccc86578cd67a680061a881e038c5e0f7c99
Gustavo Henrique <ymooncake@gmail.com>
2022-09-03 12:13:56 -0300

    not much

--- MetroidVania/Assets/Scripts/Player.gd
+++ MetroidVania/Assets/Scripts/Player.gd
@@ -7,6 +7,7 @@ var jumpForce = -720#força do pulo(gavidade-forçapulo=pulo)
 var isGrounded#variavel que verifica se ta no chão
 onready var raycasts = $raycasts#acessando o nó raycast(no que checa o chão)
 
+
 func _physics_process(delta):#função que ocorre o tempo todo
 	_get_input()#chamada da função que pega os inputs
 	_set_animation()#chamada da função que escolhe as animções
@@ -30,7 +31,6 @@ func _input(event):#função de pulo
 	if(event.is_action_pressed("ui_up")and isGrounded):#se foi pressionada e está no chão
 		velocity.y = jumpForce/2#calculo do pulo
 
-
 func _check_is_grounded():#função de checagem do pulo
 	for raycast in raycasts.get_children():#acessando os nós do raycast
 		if raycast.is_colliding():#se estiver tocando o chão retorna verdade, possibilitando o pulo
@@ -40,6 +40,7 @@ func _check_is_grounded():#função de checagem do pulo
 
 func _set_animation(): #função que seta as animações
 	var anim = "idleAnim"
+	#var jumpEfect = "jumpEfect"
 	if !isGrounded:#se estiver no ar(diferente de no chão) seta a animação de pulo
 		anim = "jumpAnim"
 	elif velocity.x != 0:#se a velocidade for diferente de 0 seta a animação de corrida
